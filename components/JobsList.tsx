6b562e977a64f636a8dae680448f2bb1bf1a702c
Aymen <aymenghaloua@gmail.com>
2024-06-12 02:40:46 +0100

    Wrapping Up!

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -5,7 +5,7 @@ import { getAllJobsAction } from '@/utils/action';
 import { useQuery } from '@tanstack/react-query';
 import ButtonContainer from './ButtonContainer';
 import ComplexButtonContainer from './ComplexButtonContainer';
-function JobsList() {
+const JobsList = () => {
   const searchParams = useSearchParams();
 
   const search = searchParams.get('search') || '';
@@ -22,25 +22,21 @@ function JobsList() {
   const page = data?.page || 0;
   const totalPages = data?.totalPages || 0;
 
-  if (isPending) return <h2 className='text-xl'>Please Wait...</h2>;
+  if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
 
-  if (jobs.length < 1) return <h2 className='text-xl'>No Jobs Found...</h2>;
+  if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
   return (
     <>
-      <div className='flex items-center justify-between mb-8'>
-        <h2 className='text-xl font-semibold capitalize '>
-          {count} jobs found
-        </h2>
-        {totalPages < 2 ? null : (
-          <ComplexButtonContainer currentPage={page} totalPages={totalPages} />
-        )}
+      <div className="flex items-center justify-between mb-8">
+        <h2 className="text-xl font-semibold capitalize ">{count} jobs found</h2>
+        {totalPages < 2 ? null : <ComplexButtonContainer currentPage={page} totalPages={totalPages} />}
       </div>
-      <div className='grid md:grid-cols-2  gap-8'>
+      <div className="grid md:grid-cols-2  gap-8">
         {jobs.map((job) => {
           return <JobCard key={job.id} job={job} />;
         })}
       </div>
     </>
   );
-}
+};
 export default JobsList;
